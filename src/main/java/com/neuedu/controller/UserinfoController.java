ea8a5315b87332e891a7db76c6d2e3478bead015
yami2333 <18521538409@163.com>
2019-05-29 19:23:25 +0800

    SSM搭建

--- src/main/java/com/neuedu/controller/UserinfoController.java
+++ src/main/java/com/neuedu/controller/UserinfoController.java
@@ -1,5 +1,6 @@
 package com.neuedu.controller;
 
+import com.neuedu.pojo.Userinfo;
 import com.neuedu.service.UserinfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,20 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @RestController
 @RequestMapping("user")
 public class UserinfoController {
-
     @Autowired
     UserinfoService userinfoService;
 
     @RequestMapping("emailCheck")
     public void checkUserEmail(String email, HttpServletResponse response) throws IOException {
-        int checknum = userinfoService.userEmailCheck(email);
+        int checknum = userinfoService.userEmailCheck();
         response.getWriter().println(checknum);
     }
 
     @PostMapping("reg")
-    public  String userReg()
+    public  String userReg(String email, String passdword, String nickname){
+
+        Userinfo userinfo = new Userinfo();
+        return "redirect:/go/login";
+    }
 }
--- src/main/java/com/neuedu/pojo/Userinfo.java
+++ src/main/java/com/neuedu/pojo/Userinfo.java
@@ -21,3 +21,4 @@ public class Userinfo {
     private int kissnum;
     private int isexsits;
 }
+
